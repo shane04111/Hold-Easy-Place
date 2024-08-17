dd0fcc9336f89ba99f5766bdaf9b0ae48b51899c
shane0411 <shane97213@gmail.com>
2024-08-18 05:41:02 +0800

    feat: all versions

--- versions/1.16.5/src/main/java/com/shane/HoldEasyPlace/mixin/MixinWorldUtils.java
+++ versions/1.16.5/src/main/java/com/shane/HoldEasyPlace/mixin/MixinWorldUtils.java
@@ -22,26 +22,12 @@
  * SOFTWARE.
  */
 
-package com.shane.HoldEasyPlace.config;
+package com.shane.HoldEasyPlace.mixin;
 
-import com.google.common.collect.ImmutableList;
-import fi.dy.masa.malilib.config.IConfigBase;
-import fi.dy.masa.malilib.config.options.ConfigBoolean;
+import fi.dy.masa.litematica.util.WorldUtils;
+import org.spongepowered.asm.mixin.Mixin;
 
-public class ConfigExtend {
-    public static class Generic {
-        public static final ConfigBoolean HOLD_EASY_PLACE = InitialBoolean("holdEasyPlace", false);
-        public static final ConfigBoolean DISABLE_HOLD_EASY_PLACE_FAIL_TIP = InitialBoolean("disableHoldEasyPlaceFailTip", true);
-        public static final ConfigBoolean ALWAYS_DISABLE_FAIL_TIP = InitialBoolean("alwaysDisableFailTip", false);
+@Mixin(WorldUtils.class)
+public class MixinWorldUtils {
 
-        public static final ImmutableList<IConfigBase> EXTENDED_OPTIONS = ImmutableList.of(
-                HOLD_EASY_PLACE,
-                DISABLE_HOLD_EASY_PLACE_FAIL_TIP,
-                ALWAYS_DISABLE_FAIL_TIP
-        );
-
-        private static ConfigBoolean InitialBoolean(String name, boolean defaultValue) {
-            return new ConfigBoolean("holdeasyplace.generic." + name, defaultValue, "holdeasyplace.generic." + name + ".comment");
-        }
-    }
 }
--- versions/1.17.1/src/main/java/com/shane/HoldEasyPlace/mixin/MixinWorldUtils.java
+++ versions/1.17.1/src/main/java/com/shane/HoldEasyPlace/mixin/MixinWorldUtils.java
@@ -25,29 +25,34 @@
 package com.shane.HoldEasyPlace.mixin;
 
 import com.shane.HoldEasyPlace.config.ConfigExtend;
-import fi.dy.masa.litematica.config.Configs;
 import fi.dy.masa.litematica.util.WorldUtils;
+import fi.dy.masa.malilib.gui.Message;
+import fi.dy.masa.malilib.util.InfoUtils;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.Screen;
-import org.jetbrains.annotations.Nullable;
+import net.minecraft.util.ActionResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(MinecraftClient.class)
-public class MixinMinecraftClient {
+@Mixin(WorldUtils.class)
+public class MixinWorldUtils {
     @Shadow
-    @Nullable
-    public Screen currentScreen;
+    private static ActionResult doEasyPlaceAction(MinecraftClient mc) {
+        return null;
+    }
 
-    @Inject(method = "tick", at = @At("HEAD"))
-    private void onHoldEasyPlace(CallbackInfo ci) {
-        MinecraftClient mc = (MinecraftClient) (Object) this;
-        boolean onHoldEasyPlace = ConfigExtend.Generic.HOLD_EASY_PLACE.getBooleanValue() && Configs.Generic.EASY_PLACE_MODE.getBooleanValue();
-        if (this.currentScreen == null && mc.player != null && onHoldEasyPlace) {
-            WorldUtils.handleEasyPlace(mc);
+
+    @Inject(method = "handleEasyPlace", at = @At("HEAD"), cancellable = true)
+    private static void handleEasyPlace(MinecraftClient mc, CallbackInfoReturnable<Boolean> cir) {
+        ActionResult result = doEasyPlaceAction(mc);
+        boolean checkTip = (ConfigExtend.Generic.DISABLE_HOLD_EASY_PLACE_FAIL_TIP.getBooleanValue() && ConfigExtend.Generic.HOLD_EASY_PLACE.getBooleanValue()) || ConfigExtend.Generic.ALWAYS_DISABLE_FAIL_TIP.getBooleanValue();
+        if (!checkTip && result == ActionResult.FAIL) {
+            InfoUtils.showGuiOrInGameMessage(Message.MessageType.WARNING, "litematica.message.easy_place_fail");
+            cir.setReturnValue(true);
+        } else {
+            cir.setReturnValue(result != ActionResult.PASS);
         }
     }
 }
