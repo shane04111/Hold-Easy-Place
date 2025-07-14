ffd18857b8d155491563e166fda187c760802240
shane0411 <shane97213@gmail.com>
2025-07-14 15:03:22 +0800

    feat: add open Schematics folder button

--- src/main/java/com/shane/HoldEasyPlace/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/shane/HoldEasyPlace/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,55 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 Shane0411
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.shane.HoldEasyPlace.mixin;
 
-public class MixinGuiMainMenu {
+import com.shane.HoldEasyPlace.util.ButtonListenerOpenFolder;
+import fi.dy.masa.litematica.gui.GuiMainMenu;
+import fi.dy.masa.malilib.gui.GuiBase;
+import fi.dy.masa.malilib.gui.button.ButtonGeneric;
+import fi.dy.masa.malilib.util.StringUtils;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = GuiMainMenu.class, remap = false)
+public abstract class MixinGuiMainMenu extends GuiBase {
+    @Shadow
+    protected abstract int getButtonWidth();
+
+    @ModifyArg(method = "initGui", at = @At(value = "INVOKE", target = "Lfi/dy/masa/litematica/gui/GuiMainMenu;createChangeMenuButton(IIILfi/dy/masa/litematica/gui/GuiMainMenu$ButtonListenerChangeMenu$ButtonType;)V", ordinal = 6), index = 1)
+    private int createSchematicManagerButton(int y) {
+        return y - 22;
+    }
+
+    @Inject(method = "initGui", at = @At("RETURN"))
+    private void initGui(CallbackInfo ci) {
+        int width = this.getButtonWidth();
+        ButtonGeneric button = new ButtonGeneric(width + 32, 74, width, 20, StringUtils.translate("holdeasyplace.gui.button.openFolder"));
+        this.addButton(button, new ButtonListenerOpenFolder());
+    }
 }
--- src/main/java/com/shane/HoldEasyPlace/util/ButtonListenerOpenFolder.java
+++ src/main/java/com/shane/HoldEasyPlace/util/ButtonListenerOpenFolder.java
@@ -1,4 +1,38 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 Shane0411
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.shane.HoldEasyPlace.util;
 
-public class ButtonListenerOpenFolder {
+
+import fi.dy.masa.litematica.data.DataManager;
+import fi.dy.masa.malilib.gui.button.ButtonBase;
+import fi.dy.masa.malilib.gui.button.IButtonActionListener;
+import net.minecraft.util.Util;
+
+public class ButtonListenerOpenFolder implements IButtonActionListener {
+    @Override
+    public void actionPerformedWithButton(ButtonBase button, int mouseButton) {
+        Util.getOperatingSystem().open(DataManager.getSchematicsBaseDirectory());
+    }
 }
